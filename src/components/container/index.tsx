b06a0960ebe4a6da08f9a81b3608efa8b9c44326
Manoj Sethi <manojsethi90@gmail.com>
2024-11-19 16:06:14 +0530

    container updated & env example file added

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styles from './index.module.scss'
 
 const Container = ({ children }: { children: React.ReactNode }) => {
-    return <div className={styles.container}>{children}</div>
+    return (
+        <div className={styles.centerContainer}>
+            <div className={styles.container}>{children}</div>
+        </div>
+    )
 }
 
 export default Container
